ad5cccfbcf1310c58f7e5aa152095d8a80f29ec8
hbauhvagner <hbauhvagner@gmail.com>
2023-01-21 19:10:00 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import { Header } from './components/Header';
 import { Post } from './Post';
 
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-        author="Vagner Nascimento" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, cum nisi, fugit veniam excepturi magnam totam quis, tempora aliquam ullam commodi vel. Veniam eaque voluptatibus dolores ducimus in quae velit?" 
-      />
-      <Post
-        author="Joana D'Arc"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <mai>
+          <Post 
+            author="Vagner Nascimento" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, cum nisi, fugit veniam excepturi magnam totam quis, tempora aliquam ullam commodi vel. Veniam eaque voluptatibus dolores ducimus in quae velit?" 
+          />
+          <Post
+            author="Joana D'Arc"
+            content="Um novo post muito legal"
+          />
+        </mai>
+      </div>
     </div>
   )
 }
