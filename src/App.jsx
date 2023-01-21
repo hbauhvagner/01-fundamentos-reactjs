147bc0d4f025bb1378aca1cbb20d9ffa4260886e
hbauhvagner <hbauhvagner@gmail.com>
2023-01-20 23:49:45 -0300

    Iniciando com React - Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-function App() {
+import { Post } from './Post';
+
+export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
   )
 }
-
-export default App
