ca9f8e9cd1dbb888aab486768bb5543639c0c2c0
hbauhvagner <hbauhvagner@gmail.com>
2023-01-21 00:01:56 -0300

    Iniciando com React - Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ export function App() {
 
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Vagner Nascimento" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, cum nisi, fugit veniam excepturi magnam totam quis, tempora aliquam ullam commodi vel. Veniam eaque voluptatibus dolores ducimus in quae velit?" 
+      />
+      <Post
+        author="Joana D'Arc"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
