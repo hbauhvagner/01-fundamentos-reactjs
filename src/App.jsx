e627bc092cdc7159c1985baebcfe23a0aee04556
hbauhvagner <hbauhvagner@gmail.com>
2023-01-21 00:39:01 -0300

    Estrutura da aplicação - CSS Module

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
 
-export function App() {
+import './styles.css'
 
+export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Vagner Nascimento" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, cum nisi, fugit veniam excepturi magnam totam quis, tempora aliquam ullam commodi vel. Veniam eaque voluptatibus dolores ducimus in quae velit?" 
