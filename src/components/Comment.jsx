b363ed604db5520fab11556f8997fac06f8f85ba
hbauhvagner <hbauhvagner@gmail.com>
2023-01-22 23:29:03 -0300

    Estrutura da aplicação - Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/hbauhvagner.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Vagner Nascimento</strong>
+                            <time title='11 de maio às 08:13h' dateTime="2022-05-11">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button type="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
